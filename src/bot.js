ccecf1b9f63e030045018d6316a848817a8c7809
Frankcs96 <fcsuarez96@gmail.com>
2020-01-11 22:28:06 +0100

    Testing ping to the bot

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,16 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log("Logged in as " + client.user.tag)
+});
 
+client.on('message', msg => {
+   if (msg.content === 'ping') {
+       msg.channel.send('pong');
+   }
+
+});
 
 
 
@@ -11,7 +20,7 @@ const client = new Discord.Client();
 
 
 client.login(process.env.TOKEN).then(r =>
-    console.log(r)
+    console.log("Token correct")
 ).catch(e => {
     console.log("Incorrect token...");
 });
